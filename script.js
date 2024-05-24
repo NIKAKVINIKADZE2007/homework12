967d759dac5cf72ce4d89510f3ed356435ad622e
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-05-24 17:41:00 +0400

    nawilis dawera

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 let url = 'https://api.quotable.io/random';
 
-fetch(url).then((data)=> )
+fetch(url)
+  .then((data) => data.json())
+  .then((responsive) => {
+    let quote = document.createElement('p');
+    quote.textContent = responsive.content;
+    quote.id = 'author';
+
+    let author = document.createElement('p');
+    author.textContent = responsive.author;
+    author.id = 'quote';
+
+    let div = document.createElement('div');
+    div.appendChild(author);
+    div.appendChild(quote);
+    document.body.appendChild(div);
+  });
