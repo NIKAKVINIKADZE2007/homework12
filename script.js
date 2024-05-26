a34e6f2d329e1a57ee91fc5793a348b7738ce92b
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-05-26 20:49:13 +0400

    finish
    damtavreba

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ let url = 'https://api.quotable.io/random';
 fetch(url)
   .then((data) => data.json())
   .then((responsive) => {
-    let quote = document.createElement('p');
+    let quote = document.createElement('h1');
     quote.textContent = responsive.content;
-    quote.id = 'author';
+    quote.id = 'quote';
 
-    let author = document.createElement('p');
+    let author = document.createElement('h1');
     author.textContent = responsive.author;
-    author.id = 'quote';
+    author.id = 'author';
 
-    let div = document.createElement('div');
-    div.appendChild(author);
-    div.appendChild(quote);
-    document.body.appendChild(div);
+    document.body.appendChild(quote);
+    document.body.appendChild(author);
   });
